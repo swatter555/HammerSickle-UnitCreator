dcad566314412ec419fd1c23dea383ee19f6ce46
@swatter555 <coderbob0101@hotmail.com>
2025-06-24 17:14:47 -0500

    Fixed broken forms

--- ViewModels/Tabs/CombatUnitsTabViewModel.cs
+++ ViewModels/Tabs/CombatUnitsTabViewModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace HammerSickle.UnitCreator.ViewModels.Tabs
 {
-    internal class CombatUnitsTabViewModel
+    public class CombatUnitsTabViewModel
     {
     }
 }
--- ViewModels/Tabs/WeaponSystemsTabViewModel.cs
+++ ViewModels/Tabs/WeaponSystemsTabViewModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace HammerSickle.UnitCreator.ViewModels.Tabs
 {
-    internal class WeaponSystemsTabViewModel
+    public class WeaponSystemsTabViewModel
     {
     }
 }
